a9f46eb2fe2baf94c2db7143a19e0d4d3b048538
Raunak Verma <69701767+REVSIX@users.noreply.github.com>
2025-05-26 20:14:45 -0400

    5/26 requires testing

--- scripts/utils/stop.py
+++ scripts/utils/stop.py
@@ -1,8 +1,8 @@
 import logging
 import threading
 
-stop_event = threading.Event()  # Event to handle graceful shutdown
+event = threading.Event()  # Event to handle graceful shutdown
 
 def signal_handler(signum, frame):
     logging.info("Received termination signal. Exiting gracefully...")
-    stop_event.set()  # Signal the loop to stop
+    event.set()  # Signal the loop to stop
